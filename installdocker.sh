f433507705dd6b31ff2b9ba29bb2a424e1ac2b10
Andrew Pruski <dbafromthecold@gmail.com>
2017-08-01 15:48:00 +0100

    Add files via upload

---
+++ installdocker.sh
@@ -0,0 +1,33 @@
+# install docker community edition
+sudo apt-get update -y
+
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+
+sudo apt-get update -y
+
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update -y
+
+sudo apt-get install -y docker-ce
+
+
+# install sql command line tools
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+
+sudo apt-get update -y
+
+sudo apt-get install -y mssql-tools unixodbc-dev
+
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
